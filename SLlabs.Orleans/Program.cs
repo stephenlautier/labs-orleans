d85d0ade8cc5007235a2bbdc87c05cad983036ab
Stephen Lautier <stephen.lautier@outlook.com>
2017-04-10 20:49:20 +0200

    Add project files.

---
+++ SLlabs.Orleans/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SLlabs.Orleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
