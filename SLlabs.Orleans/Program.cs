3f97ff8023c96a507ebb76fbbdf93c40fc3423e7
Stephen Lautier <stephen.lautier@outlook.com>
2017-04-10 20:53:57 +0200

    initial commit

--- SLlabs.Orleans/Program.cs
+++ SLlabs.Orleans/Program.cs
@@ -2,11 +2,12 @@
 
 namespace SLlabs.Orleans
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
 }
