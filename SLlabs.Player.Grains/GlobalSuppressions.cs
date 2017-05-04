249dca38a0654c8898f8948938bbfa3faacf8f26
Stephen Lautier <stephen.lautier@outlook.com>
2017-05-04 14:12:32 +0200

    added suppressions for u2u for string concat

---
+++ SLlabs.Player.Grains/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "U2U1105:Do not use string interpolation to concatenate strings", Justification = "<Pending>", Scope = "member", Target = "~M:SLlabs.Player.Grains.PlayerGrain.OnActivateAsync~System.Threading.Tasks.Task")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "U2U1105:Do not use string interpolation to concatenate strings", Justification = "<Pending>", Scope = "member", Target = "~M:SLlabs.Player.Grains.PlayerGrain.Say(System.String)~System.Threading.Tasks.Task{System.String}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "U2U1105:Do not use string interpolation to concatenate strings", Justification = "<Pending>", Scope = "member", Target = "~M:SLlabs.Player.Grains.PlayerGrain.SetAlias(System.String)~System.Threading.Tasks.Task")]
+
